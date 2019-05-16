f956c7a5392c2222ffe7ef5a58178d5a99286be7
kevin <k57764328@vmage.com.tw>
2019-05-16 19:10:00 +0800

    revise permission control

--- src/main/java/com/pinpinbox/android/Views/DraggerActivity/DraggerScreen/DraggerActivity.java
+++ src/main/java/com/pinpinbox/android/Views/DraggerActivity/DraggerScreen/DraggerActivity.java
@@ -4,21 +4,32 @@ package com.pinpinbox.android.Views.DraggerActivity.DraggerScreen;
  * Created by kevin9594 on 2016/8/28.
  */
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import com.pinpinbox.android.R;
 import com.pinpinbox.android.Utility.HttpUtility;
+import com.pinpinbox.android.Utility.SystemUtility;
 import com.pinpinbox.android.Views.DraggerActivity.DraggerMargin.SwipeBackActivityHelper;
 import com.pinpinbox.android.Views.DraggerActivity.NoDragger.NoDraggerLayout;
 import com.pinpinbox.android.Views.DraggerActivity.SwipeBackActivityInterface;
 import com.pinpinbox.android.Views.DraggerActivity.SwipeBackLayout;
 import com.pinpinbox.android.pinpinbox2_0_0.custom.LoadingAnimation;
+import com.pinpinbox.android.pinpinbox2_0_0.custom.stringClass.DialogStyleClass;
 import com.pinpinbox.android.pinpinbox2_0_0.custom.widget.NoConnect;
 import com.pinpinbox.android.pinpinbox2_0_0.custom.widget.StatusControl;
+import com.pinpinbox.android.pinpinbox2_0_0.dialog.CheckExecute;
+import com.pinpinbox.android.pinpinbox2_0_0.dialog.DialogV2Custom;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
@@ -31,7 +42,6 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
     private LoadingAnimation loading;
     private NoConnect noConnect;
     private StatusControl statusControl;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,7 +58,6 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
         loading = new LoadingAnimation(this);
 
     }
-
 
     private void initSwipeBack() {
         backActivityHelper = new DraggerBackHelper(this);
@@ -86,7 +95,6 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
         }
     }
 
-
     public void dissmissLoading() {
         if (loading != null) {
             loading.dismiss();
@@ -96,7 +104,6 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
     public LoadingAnimation getLoading() {
         return this.loading;
     }
-
 
     public void setStatusDark() {
 
@@ -117,7 +124,6 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
         }
 
     }
-
 
     public void setStatusColor(int color){
 
@@ -151,13 +157,87 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
 
     }
 
-
-
     public void cancelTask(AsyncTask task) {
         if (task != null && !task.isCancelled()) {
             task.cancel(true);
         }
     }
+
+
+
+
+    private int REQUEST_CODE = 1;
+    private CheckPermissionCallBack callBack;
+    private int message;
+    private String permission;
+
+
+    public interface CheckPermissionCallBack{
+
+        void success();
+
+    }
+
+    public void commonCheckPermission(String permission, int request_code, int message, CheckPermissionCallBack callBack){
+        this.permission = permission;
+        this.REQUEST_CODE = request_code;
+        this.callBack = callBack;
+        this.message = message;
+
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            //尚未授權
+            ActivityCompat.requestPermissions(this, new String[]{permission}, request_code);
+
+        } else {
+
+            //已授權
+            callBack.success();
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+        if (requestCode == REQUEST_CODE) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        callBack.success();
+                    }
+                }, 500);
+
+            }else {
+
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(mActivity, permission)) {
+
+                    DialogV2Custom d = new DialogV2Custom(mActivity);
+                    d.setStyle(DialogStyleClass.CHECK);
+                    d.getTvRightOrBottom().setText(R.string.pinpinbox_2_0_0_dialog_setting);
+                    d.setMessage(message);
+                    d.setCheckExecute(new CheckExecute() {
+                        @Override
+                        public void DoCheck() {
+                            SystemUtility.getAppDetailSettingIntent(mActivity);
+                        }
+                    });
+                    d.show();
+
+                }
+
+            }
+
+        }
+
+    }
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -200,13 +280,11 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 
-
     @Override
     public void onResume() {
 
         super.onResume();
     }
-
 
     @Override
     protected void onRestart() {
@@ -228,16 +306,5 @@ public abstract class DraggerActivity extends FragmentActivity implements SwipeB
 
         super.onDestroy();
     }
-
-
-    //    public View getRootView() {
-////        return ((ViewGroup)this.findViewById(android.R.id.content)).getChildAt(0);
-//
-////        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-////        getWindow().getDecorView().setBackgroundDrawable(null);
-//
-//
-//        return getWindow().getDecorView().findViewById(android.R.id.content);
-//    }
 
 }
